d69ca3974b217793960a11973b08771907eaa254
Juno-ko <juno-koo@pm.me>
2024-01-22 12:38:23 +0200

    Added assets and started doing signup-form

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,6 +1,31 @@
+<<<<<<< HEAD
+=======
+import { Outlet, Navigate } from 'react-router-dom';
+
+>>>>>>> a9670b8 (Added assets and started doing signup-form)
 const AuthLayout = () => {
+  const isAuthenticated = false;
+
   return (
-    <div>AuthLayout</div>
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ): (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col 
+          py-10">
+            <Outlet />
+          </section>
+          <img
+            src="/assets/images/side-img.svg"
+            alt="logo"
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
+        </>
+      )
+      
+      }
+    </>
   )
 }
 
--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,6 +1,8 @@
+import { Button } from "@/components/ui/button"
+
 const SignupForm = () => {
   return (
-    <div>SignupForm</div>
+    <Button>Click me</Button>
   )
 }
 
