7e06a11ba80f1bd5aba478b63e4251332aee3f44
Juno-ko <juno-koo@pm.me>
2024-02-05 14:36:49 +0200

    PostForm created.

--- src/_root/pages/CreatePost.tsx
+++ src/_root/pages/CreatePost.tsx
@@ -1,3 +1,5 @@
+import PostForm from "@/components/forms/PostForm"
+
 const CreatePost = () => {
 	return (
 		<div className="flex flex-1">
@@ -11,6 +13,8 @@ const CreatePost = () => {
 					/>
 					<h2 className="h3-bold md:h2-bold text-left w-full">Create Post</h2>
 				</div>
+
+				<PostForm />
 			</div>
 		</div>
 	)
