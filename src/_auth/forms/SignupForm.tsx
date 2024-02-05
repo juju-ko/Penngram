246078b92d5d1b74f0faf79bc1e1cd0c2649042f
Juno-ko <juno-koo@pm.me>
2024-02-05 14:38:18 +0200

    Tiny update

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -12,7 +12,7 @@ import Loader from "@/components/shared/Loader"
 import { useCreateUserAccount, useSignInAccount } from "@/lib/react-query/queriesAndMutations"
 import { useUserContext } from "@/context/AuthContext"
  
-const SignupForm = () => { // 2:38:13 !!!!!!!!!!!!!!!!!!!!
+const SignupForm = () => { // 2:51:53 !!!!!!!!!!!!!!!!!!!!
   const { toast } = useToast();
   const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
   const navigate = useNavigate();
