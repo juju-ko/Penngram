0b213517c39a6e336da126be18a3329b977f895e
Juno-ko <juno-koo@pm.me>
2024-02-08 13:57:37 +0200

    FileUploader done and tiny tweak

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -12,7 +12,7 @@ import Loader from "@/components/shared/Loader"
 import { useCreateUserAccount, useSignInAccount } from "@/lib/react-query/queriesAndMutations"
 import { useUserContext } from "@/context/AuthContext"
  
-const SignupForm = () => { // 2:51:53 !!!!!!!!!!!!!!!!!!!!
+const SignupForm = () => { // 3:09:21 !!!!!!!!!!!!!!!!!!!!
   const { toast } = useToast();
   const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
   const navigate = useNavigate();
--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -32,9 +32,16 @@ const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
       <input {...getInputProps()} className="cursor-pointer" />
       {
         fileUrl ? (
-					<div>
-						test 1
-					</div>
+					<>
+						<div className="flex flex-1 justify-center w-full p-5 lg:p-10">
+							<img
+								src={fileUrl}
+								alt="image"
+								className="file_uploader-img"
+							/>
+						</div>
+							<p className="file_uploader-label">Click or drag photo to replace</p>
+					</>
 				) : (
 					<div className="file_uploader-box">
 						<img 
