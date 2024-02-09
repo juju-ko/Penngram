75ddead0394af7bedfba546c546844d08e92cbff
Juno-ko <juno-koo@pm.me>
2024-02-09 17:29:30 +0200

    CreatePost validation works

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -3,38 +3,33 @@ import { useForm } from "react-hook-form"
 import { z } from "zod"
 
 import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "../ui/textarea"
 import FileUploader from "../shared/FileUploader"
- 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
+import { PostValidation } from "@/lib/validation"
+import { Models } from "appwrite"
 
-const PostForm = ( post) => {
+type PostFormProps = {
+	post?: Models.Document;
+}
+
+const PostForm = ({ post }: PostFormProps) => {
 	// 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof PostValidation>>({
+    resolver: zodResolver(PostValidation),
     defaultValues: {
-      username: "",
+      caption: post ? post?.caption : "",
+			file: [],
+			location: post ? post?.location : "",
+			tags: post ? post.tags.join(',') : ''
     },
   })
- 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: z.infer<typeof PostValidation>) {
     // Do something with the form values.
-    // ✅ This will be type-safe and validated.
+    // This will be type-safe and validated.
     console.log(values)
   }
 
@@ -78,7 +73,7 @@ const PostForm = ( post) => {
             <FormItem>
               <FormLabel className="shad-form_label">Add Location</FormLabel>
               <FormControl>
-                <Input type="text" className="shad-input" />
+                <Input type="text" className="shad-input" {...field}/>
               </FormControl>
               <FormMessage className="shad-form_message" />
             </FormItem>
@@ -95,6 +90,7 @@ const PostForm = ( post) => {
 									type="text" 
 									className="shad-input"
 									placeholder="Dogs, Cats, Cuties"
+									{...field}
 								/>
               </FormControl>
               <FormMessage className="shad-form_message" />
