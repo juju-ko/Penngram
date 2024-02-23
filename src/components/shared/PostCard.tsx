b84dbdf554e15aa605216092ee3881abaf1bae7e
juno-ko <juno-koo@pm.me>
2024-02-23 15:38:14 +0200

    PostCard component now shows caption, location and tags.

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -1,3 +1,4 @@
+import { useUserContext } from "@/context/AuthContext";
 import { multiFormatDateString } from "@/lib/utils";
 import { Models } from "appwrite";
 import { Link } from 'react-router-dom';
@@ -7,6 +8,10 @@ type PostCardProps = {
 }
 
 const PostCard = ({ post }: PostCardProps) => {
+	const { user } = useUserContext();
+
+	if (!post.creator) return;
+
 	return (
 		<div className="post-card">
 			<div className="flex-between">
@@ -36,12 +41,33 @@ const PostCard = ({ post }: PostCardProps) => {
 					</div>
 				</div>
 
-				<Link to={`/update-post/${post.$id}`}>
+				<Link to={`/update-post/${post.$id}`}
+					className={`${user.id !== post.creator.$id && "hidden"}`}
+				>
 					<img
 						src="/assets/icons/edit.svg" alt="edit" width={20} height={20}
 					/>
 				</Link>
 			</div>
+
+			<Link to={`/posts/${post.$id}`}>
+				<div className="small-medium lg:base-medium py-5">
+					<p>{post.caption}</p>
+					<ul className="flex gap-1 mt-2">
+						{post.tags.map((tag: string) => (
+							<li key={tag} className="text-light-3">
+								#{tag}
+							</li>
+						))}
+					</ul>
+				</div>
+
+				<img
+					src={post.imageUrl || '/assets/icons/profile-placeholder.svg'} 
+					className="post-card_img"
+					alt="post-image"
+				/>
+			</Link>
 		</div>
 	)
 }
