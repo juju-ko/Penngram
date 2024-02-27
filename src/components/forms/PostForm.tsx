4cafa3fa5a7e7c588a1f1ceafdcf1d826ff889c9
Juno-ko <juno-koo@pm.me>
2024-02-27 17:59:05 +0200

    Updating post working now. Remember to fix button.

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -13,14 +13,17 @@ import { PostValidation } from "@/lib/validation"
 import { Models } from "appwrite"
 import { useUserContext } from "@/context/AuthContext"
 import { useToast } from "../ui/use-toast"
-import { useCreatePost } from "@/lib/react-query/queriesAndMutations"
+import { useCreatePost, useUpdatePost } from "@/lib/react-query/queriesAndMutations"
 
 type PostFormProps = {
 	post?: Models.Document;
+	action: 'Create' | 'Update';
 }
 
-const PostForm = ({ post }: PostFormProps) => {
+const PostForm = ({ post, action }: PostFormProps) => {
 	const { mutateAsync: createPost, isPending: isLoadingCreate } = useCreatePost();
+	const { mutateAsync: updatePost, isPending: isLoadingUpdate } = useUpdatePost();
+
 	const { user } = useUserContext();
 	const { toast } = useToast();
 	const navigate = useNavigate();
@@ -37,7 +40,21 @@ const PostForm = ({ post }: PostFormProps) => {
   })
   // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof PostValidation>) {
-    const newPost = await createPost({
+    if (post && action === 'Update') {
+			const updatedPost = await updatePost({
+				...values,
+				postId: post.$id,
+				imageId: post?.imageId,
+				imageUrl: post?.imageUrl,
+			})
+			if (!updatedPost) {
+				toast({ title: 'Please try again'})
+			}
+
+			return navigate(`/posts/${post.$id}`)
+		}
+		
+		const newPost = await createPost({
 			...values,
 			userId: user.id,
 		})
@@ -73,7 +90,7 @@ const PostForm = ({ post }: PostFormProps) => {
           name="file"
           render={({ field }) => (
             <FormItem>
-              <FormLabel className="shad-form_label">Caption</FormLabel>
+              <FormLabel className="shad-form_label">Add Photo</FormLabel>
               <FormControl>
                 <FileUploader 
 									fieldChange={field.onChange}
@@ -125,8 +142,11 @@ const PostForm = ({ post }: PostFormProps) => {
 					<Button 
 						type="submit"
 						className="shad-button_primary whitespace-nowrap"
+						disabled={isLoadingCreate || isLoadingUpdate}
+
 					>
-						Submit
+						{isLoadingCreate || isLoadingUpdate && 'Loading...'}
+						{action} Post
 					</Button>
 				</div>
       </form>
