6fa5c8edb320913723d8e3a5168b61cc7c620e6f
Juno-ko <juno-koo@pm.me>
2024-02-28 15:09:39 +0200

    Started working with post details page

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import Loader from "@/components/shared/Loader";
+import { useGetPostById } from "@/lib/react-query/queriesAndMutations"
+import { multiFormatDateString } from "@/lib/utils";
+import { useParams, Link } from "react-router-dom"
 
 const PostDetails = () => {
+	const { id } = useParams();
+	const { data: post, isPending } = useGetPostById(id || '');
+
 	return (
-		<div>PostDetails</div>
+		<div className="post_details-container">
+			{isPending ? <Loader /> : (
+				<div className="post_details-card">
+					<img
+						src={post?.imageUrl}
+						alt="post"
+						className="post_details-img" 
+					/>
+					<div className="post_details-info">
+						<div className="flex-between w-full">
+							<Link to={`/profile/${post?.creator.$id}`} 
+							className="flex gap-3">
+								<img
+									src={post?.creator?.imageUrl || 
+									'/assets/icons/profile-placeholder.svg'}
+									alt="creator"
+									className="rounded-full w-12 lg:h-12"
+								/>
+								<div className="flex flex-col">
+									<p className="base-medium lg:body-bold text-light-1">
+										{post?.creator.name}
+									</p>
+									<div className="flex-center gap-2 text-light-3">
+										<p className="subtle-semibold lg:small-regular">
+											{multiFormatDateString(post?.$createdAt)}
+										</p>
+										-
+										<p className="subtle-semibold lg:small-regular">
+											{post?.location}
+										</p>
+									</div>
+								</div>
+							</Link>
+							<div className="flex-center gap-4">
+
+							</div>
+						</div>
+					</div>
+				</div>
+			)}
+		</div>
 	)
 }
 
