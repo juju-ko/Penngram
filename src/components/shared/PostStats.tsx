a173097b1fadb4985110fab391af3fa650516ed6
Juno-ko <juno-koo@pm.me>
2024-02-28 15:34:55 +0200

    PostDetails works as intended

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -12,12 +12,12 @@ import Loader from "./Loader";
 
 
 type PostStatsProps = {
-  post: Models.Document;
+  post?: Models.Document;
   userId: string;
 };
 
 const PostStats = ({ post, userId } : PostStatsProps) => {
-	const likesList = post.likes.map((user: Models.Document) => user.$id);
+	const likesList = post?.likes.map((user: Models.Document) => user.$id);
 
 	const [likes, setLikes] = useState(likesList);
 	const [isSaved, setIsSaved] = useState(false);
@@ -29,7 +29,7 @@ const PostStats = ({ post, userId } : PostStatsProps) => {
 	const { data: currentUser } = useGetCurrentUser();
 
 	const savedPostRecord = currentUser?.save.find(
-		(record: Models.Document) => record.post.$id === post.$id
+		(record: Models.Document) => record.post.$id === post?.$id
 	);
 
 	useEffect(() => {
@@ -50,7 +50,7 @@ const PostStats = ({ post, userId } : PostStatsProps) => {
     }
 
     setLikes(likesArray);
-    likePost({ postId: post.$id, likesArray });
+    likePost({ postId: post?.$id || '', likesArray });
   };
 	
 	const handleSavePost = (
@@ -62,7 +62,7 @@ const PostStats = ({ post, userId } : PostStatsProps) => {
       setIsSaved(false);
       deleteSavedPost(savedPostRecord.$id);
     } else {
-			savePost({ postId: post.$id, userId });
+			savePost({ postId: post?.$id || '', userId });
 			setIsSaved(true);
 		}
   };
