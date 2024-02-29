1728f28d253fb38588a83e540a5b1815b369bb38
Juno-ko <juno-koo@pm.me>
2024-02-29 12:12:41 +0200

    Explore page search bar progressing

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -1,10 +1,39 @@
+import { Input } from "@/components/ui/input"
+import { useState } from "react"
+
 const Explore = () => {
+	const [searchValue, setSearchValue] = useState('');
+
 	return (
 		<div className="explore-container"> 
 			<div className="explore-inner_container">
 				<h2 className="h3-bold md:h2-bold w-full">Search Posts</h2>
 				<div className="flex gap-1 px-4 w-full rounded-lg bg-dark-4">
+					<img
+						src="/assets/icons/search.svg"
+						width={24}
+						height={24}
+						alt="search"
+					/>
+					<Input
+						type="text"
+						placeholder='Search'
+						className="explore-search"
+						value={searchValue}
+						onChange={(e) => setSearchValue(e.target.value)}
+					/>
+				</div>
+			</div>
 
+			<div className="flex-between w-full max-w-5xl mt-16 mb-7">
+				<h3 className="body-bold md:h3-bold">Popular Today</h3>
+
+				<div className="flex-center gap-3 bg-dark-3 
+				rounded-xl px-4 py-2 cursor-pointer">
+					<p className="small-medium md:base-medium text-light-2">All</p>
+					<img
+						src="/assets/icons/arrow-down.svg"
+					/>
 				</div>
 			</div>
 		</div>
