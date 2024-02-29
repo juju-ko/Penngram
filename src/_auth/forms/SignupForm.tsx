fe4a846a19501fd54918e2b9af02306174b0119d
Juno-ko <juno-koo@pm.me>
2024-02-29 12:31:55 +0200

    Bump

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -12,7 +12,7 @@ import Loader from "@/components/shared/Loader"
 import { useCreateUserAccount, useSignInAccount } from "@/lib/react-query/queriesAndMutations"
 import { useUserContext } from "@/context/AuthContext"
  
-const SignupForm = () => { // 5:01:30 !!!!!!!!!!!!!!!!!!!!
+const SignupForm = () => { // 5:11:00 !!!!!!!!!!!!!!!!!!!!
   const { toast } = useToast();
   const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
   const navigate = useNavigate();
