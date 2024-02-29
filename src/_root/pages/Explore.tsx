22fc5b220477cd6e8f2054765ddbfb7184f47522
Juno-ko <juno-koo@pm.me>
2024-02-29 12:31:33 +0200

    Added two new components

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -1,8 +1,16 @@
+import GridPostList from "@/components/shared/GridPostList";
+import SearchResults from "@/components/shared/SearchResults";
 import { Input } from "@/components/ui/input"
 import { useState } from "react"
 
 const Explore = () => {
 	const [searchValue, setSearchValue] = useState('');
+
+	//const posts = [];
+
+	//const shouldShowSearchResults = searchValue !== '';
+	//const shouldShowPosts = !shouldShowSearchResults && 
+	//posts?.pages.every((item) => item.documents.length === 0);
 
 	return (
 		<div className="explore-container"> 
@@ -32,10 +40,25 @@ const Explore = () => {
 				rounded-xl px-4 py-2 cursor-pointer">
 					<p className="small-medium md:base-medium text-light-2">All</p>
 					<img
-						src="/assets/icons/arrow-down.svg"
+						src="/assets/icons/filter.svg"
+						width={20}
+						height={20}
+						alt="filter"
 					/>
 				</div>
 			</div>
+
+			{/*<div className="flex flex-wrap gap-9 w-full max-w-5xl">
+				{shouldShowSearchResults ? (
+					<SearchResults
+
+					/>
+				) : shouldShowPosts ? (
+					<p className="text-light-4 mt-10 text-center w-full">End of Posts</p>
+				) : posts.pages.map((item, index) => (
+					<GridPostList key={`page-${index}`} posts={item.documents} />
+				))}
+				</div> */}
 		</div>
 	)
 }
