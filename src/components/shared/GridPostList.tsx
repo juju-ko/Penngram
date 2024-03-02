c340f84a84eb53febdf2d5b1b1b4ebe403a7ce4c
Juno-ko <juno-koo@pm.me>
2024-03-02 12:32:21 +0200

    Explore page coming together

--- src/components/shared/GridPostList.tsx
+++ src/components/shared/GridPostList.tsx
@@ -1,5 +1,6 @@
 import { useUserContext } from "@/context/AuthContext"
 import { Models } from "appwrite"
+import { Link } from "react-router-dom"
 
 type GridPostListProps = {
 	posts: Models.Document[]
@@ -10,7 +11,13 @@ const GridPostList = ({ posts }: GridPostListProps) => {
 	
 	return (
 		<ul className="grid-container">
-
+			{posts.map((post) => (
+				<li key={post.$id} className="relative min-w-80 h-80">{post.caption}
+					<Link to={`/posts/${post.$id}`} className="grid-post_link">
+						<img src={post.imageUrl} alt="post" className="h-full w-full object-cover" />
+					</Link>
+				</li>
+			))}
 		</ul>
 	)
 }
