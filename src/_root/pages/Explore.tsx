b239a60879e42051ce1479132c86be64c08bd7df
Juno-ko <juno-koo@pm.me>
2024-03-02 12:27:17 +0200

    GridPostList started

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -1,20 +1,30 @@
 import GridPostList from "@/components/shared/GridPostList";
+import Loader from "@/components/shared/Loader";
 import SearchResults from "@/components/shared/SearchResults";
 import { Input } from "@/components/ui/input"
-import { useSearchPosts } from "@/lib/react-query/queriesAndMutations";
+import useDebounce from "@/hooks/useDebounce";
+import { useGetPosts, useSearchPosts } from "@/lib/react-query/queriesAndMutations";
 import { useState } from "react"
 
 const Explore = () => {
+	const { data: posts, fetchNextPage, hasNextPage } = useGetPosts();
+	
 	const [searchValue, setSearchValue] = useState('');
-
+	const debouncedValue = useDebounce(searchValue, 500);
 	const { data: searchedPosts, isFetching: isSearchFetching } = 
-	useSearchPosts(searchValue);
+	useSearchPosts(debouncedValue);
 
-	//const posts = [];
+	if (!posts) {
+		return (
+			<div className="flex-center w-full h-full">
+				<Loader />
+			</div>
+		)
+	}
 
-	//const shouldShowSearchResults = searchValue !== '';
-	//const shouldShowPosts = !shouldShowSearchResults && 
-	//posts?.pages.every((item) => item.documents.length === 0);
+	const shouldShowSearchResults = searchValue !== '';
+	const shouldShowPosts = !shouldShowSearchResults && 
+	posts?.pages.every((item) => item.documents.length === 0);
 
 	return (
 		<div className="explore-container"> 
@@ -52,7 +62,7 @@ const Explore = () => {
 				</div>
 			</div>
 
-			{/*<div className="flex flex-wrap gap-9 w-full max-w-5xl">
+			<div className="flex flex-wrap gap-9 w-full max-w-5xl">
 				{shouldShowSearchResults ? (
 					<SearchResults
 
@@ -62,7 +72,7 @@ const Explore = () => {
 				) : posts.pages.map((item, index) => (
 					<GridPostList key={`page-${index}`} posts={item.documents} />
 				))}
-				</div> */}
+				</div>
 		</div>
 	)
 }
--- src/components/shared/GridPostList.tsx
+++ src/components/shared/GridPostList.tsx
@@ -1,6 +1,17 @@
-const GridPostList = () => {
+import { useUserContext } from "@/context/AuthContext"
+import { Models } from "appwrite"
+
+type GridPostListProps = {
+	posts: Models.Document[]
+}
+
+const GridPostList = ({ posts }: GridPostListProps) => {
+	const { user } = useUserContext();
+	
 	return (
-		<div>GridPostList</div>
+		<ul className="grid-container">
+
+		</ul>
 	)
 }
 
