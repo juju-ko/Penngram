335e772a06f8ca18d1cc054dd9ae0776e35f8659
Juno-ko <juno-koo@pm.me>
2024-03-03 11:20:10 +0200

    Changed login page sideimg

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -14,7 +14,7 @@ const AuthLayout = () => {
             <Outlet />
           </section>
           <img
-            src="/assets/images/side-img.svg"
+            src="/assets/images/sideimg.png"
             alt="logo"
             className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
           />
